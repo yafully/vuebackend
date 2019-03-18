147906e5cba93f298728c5be1fb192e5c3cf0030
Yafully <yafully@gmail.com>
2019-03-18 22:01:04 +0800

    add global less loader

--- vue.config.js
+++ vue.config.js
@@ -188,7 +188,14 @@ module.exports = {
   }, // 第三方插件配置
 
   pluginOptions: {
-    // ...
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [
+        //这个是加上自己的路径，
+        //注意：试过不能使用别名路径
+        //path.resolve(__dirname, './src/common/less/index.less')
+      ]
+    }
   }
 };
 
